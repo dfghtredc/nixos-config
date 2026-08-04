0955d4b2d4f8ad2226c15268e536a2964f3fc659
dfghtredc <Aa.hernandezreyes06@gmail.com>
2026-08-04 09:36:51 -0400

    Add: configured Starship with home manager

--- home/home.nix
+++ home/home.nix
@@ -7,8 +7,16 @@
   home.stateVersion = "26.05";
   
   programs.home-manager.enable = true;
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
  
   home.file.".zshrc".source = ./zsh/.zshrc;
+
+  home.file.".comfig/starship.toml".source = 
+    ./starship/starship.toml;
  
   home.file.".config/zsh" = {
     source = ./zsh/config;
