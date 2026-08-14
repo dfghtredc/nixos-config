08f1081dc76dee737a0015ecba67edb86bfa8612
dfghtredc <Aa.hernandezreyes06@gmail.com>
2026-08-14 18:00:42 -0400

    Fix & Update: Found a bug that messed with the tmux and added new plugins and controls

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -23,13 +23,14 @@
 
    extraConfig = ''
      #Terminal support
-
      set -g default-terminal "tmux-256color"
      set -as terminal-features ",xterm-256color:RGB"
      set -as terminal-features ",xterm-kitty:RGB"
 
+
      #True Color
      set -ga terminal-overrides ",*:Tc"
+
 
      #Keep New panes/windows in the current directory
      bind | split-window -h -c "#{pane_current_path}"
@@ -37,9 +38,26 @@
      bind c new-window -c "#{pane_current_path}"
 
 
-     #reload tmux configuration
-
+     #Reload tmux configuration
      bind r source-file ~/.config/tmux/tmux.conf \; display-message "tmux config reloaded"
+
+
+     #Start numbering at 1
+     set -g pane-base-index 1
+
+
+     #Automatically renumber windows after one closes
+     set -g renumber-windows on
+
+     #Vim-style pane naigation
+     bind -r H resize-pane -L 5
+     bind -r J resize-pane -D 5
+     bind -r K resize-pane -U 5
+     bind -r L resize-pane -R 5
+
+
+     #Vi-style copy mode
+     setw -g mode-keys vi    
    '';
  };
 }
