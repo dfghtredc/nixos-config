f47e0c8334a473f3c71a93efafc8c7fd1e0124fa
dfghtredc <Aa.hernandezreyes06@gmail.com>
2026-08-16 22:26:01 -0400

    Add: created custom zsh prompt with project path formatting

--- home/zsh/config/prompt.zsh
+++ home/zsh/config/prompt.zsh
@@ -2,35 +2,45 @@ autoload -Uz colors
 colors
 
 prompt_path() {
-    local path="$PWD"
+    local project="$HOME/projects/nixos-config"
 
-    if [[ "$path" == "$HOME" ]]; then
+
+    if [[ "$PWD" == "$HOME" ]]; then
         print -n '~'
         return
     fi
 
-    if [[ "$path" == "$HOME/projects/nixos-config" ]]; then
-         print -n '~/projects/nixos-config'
+
+    if [[ "$PWD" == "$project" ]]; then
+         print -n '~/nixos-config'
          return
     fi
 
-    if [[ "$path" = "$HOME/projects/nixos-config/"* ]]; then
-        local relative="${path#HOME/projects/nixos-config/}"
-        local parts=("${(@s:/:)relative}")
 
-        if (( ${#parts} <= 2 )); then
-            print -n "~/projects/nixos-config/$relative"
+    if [[ "$PWD" = "$project/"* ]]; then
+        local relative="${PWD#$project/}"
+
+        local first="${relative%%/*}"
+
+        local rest="${relative#*/}"
+
+        if [[ "$relative" == "first" ]]; then
+            print -n "~/nixos-config/$first"
+
 
         else
-            print -n "~/projects/nixos-config.${parts[-2]}/${parts[-1]}"
+            local second ="${rest%%/*}"
+
+            print -n "~/nixos-config/$first/$second"
 
         fi
+
         return
     fi
 
     print -n "%~"
 
-PROMPT='%F{white}%n@%m:%~%f %F{magenta}>>>%f '
+PROMPT='%F{white}%n@%m:%f$(prompt_path) %F{magenta}>>>%f '
 
 
 
